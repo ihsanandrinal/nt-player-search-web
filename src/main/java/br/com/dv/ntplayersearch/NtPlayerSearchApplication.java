530646aa0ee7584a8a77bebd4c7d177b36d89d4c
Douglas Vieira <douglasdotv@hotmail.com>
2023-07-30 00:19:31 -0300

    Initial commit

---
+++ src/main/java/br/com/dv/ntplayersearch/NtPlayerSearchApplication.java
@@ -0,0 +1,13 @@
+package br.com.dv.ntplayersearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtPlayerSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NtPlayerSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/dv/ntplayersearch/NtPlayerSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dv.ntplayersearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtPlayerSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
