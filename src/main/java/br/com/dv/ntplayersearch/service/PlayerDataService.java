2adc5ccb0936b8c3f6532e7232f443dbf452f007
Douglas Vieira <douglasdotv@hotmail.com>
2023-07-31 13:02:46 -0300

    Refactor PlayerDataService class

--- src/main/java/br/com/dv/ntplayersearch/service/PlayerDataService.java
+++ src/main/java/br/com/dv/ntplayersearch/service/PlayerDataService.java
@@ -3,9 +3,14 @@ package br.com.dv.ntplayersearch.service;
 import br.com.dv.ntplayersearch.model.Player;
 import br.com.dv.ntplayersearch.model.PlayerInfo;
 import br.com.dv.ntplayersearch.model.PlayerSearchForm;
+import br.com.dv.ntplayersearch.util.PlayerDataParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import javax.xml.parsers.DocumentBuilder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -19,36 +24,54 @@ import java.util.stream.IntStream;
 @Service
 public class PlayerDataService {
 
+    private final WebClient webClient;
+    private final ObjectMapper objectMapper;
+    private final DocumentBuilder documentBuilder;
     private final PlayerDataParser playerDataParser;
-    private PlayerEvaluator playerEvaluator;
-    private WebDataFetcher webDataFetcher;
+    private final Integer threadPoolSize;
 
-    public PlayerDataService() {
-        this.playerDataParser = new PlayerDataParser();
+    public PlayerDataService(
+            WebClient webClient,
+            ObjectMapper objectMapper,
+            DocumentBuilder documentBuilder,
+            PlayerDataParser playerDataParser,
+            @Value("${threadPoolSize}")
+            Integer threadPoolSize
+    ) {
+        this.webClient = webClient;
+        this.objectMapper = objectMapper;
+        this.documentBuilder = documentBuilder;
+        this.playerDataParser = playerDataParser;
+        this.threadPoolSize = threadPoolSize;
     }
 
     public List<Player> getPlayers(PlayerSearchForm form) {
-        this.playerEvaluator = new PlayerEvaluator(form.getPlayerMinSkills());
-        this.webDataFetcher = new WebDataFetcher(
+        PlayerEvaluator playerEvaluator = new PlayerEvaluator(form.getPlayerMinSkills());
+
+        DataFetcher dataFetcher = new DataFetcher(
                 form.getCountry(),
-                form.getUrl(),
+                form.getUrl().replace("sub=players", "sub=search&pid="),
                 form.getSessionId(),
                 IntStream.rangeClosed(form.getStartingLeagueId(), form.getEndingLeagueId()).boxed().toList(),
                 IntStream.rangeClosed(form.getMinAge(), form.getMaxAge()).boxed().toList(),
                 playerDataParser,
-                playerEvaluator
+                playerEvaluator,
+                webClient,
+                objectMapper,
+                documentBuilder,
+                threadPoolSize
         );
 
-        List<String> allTeamIds = webDataFetcher.getTeamIds();
-        Map<Long, PlayerInfo> thePlayers = webDataFetcher.getPlayersFromTeamIds(allTeamIds);
+        List<String> allTeamIds = dataFetcher.getTeamIds();
+        Map<String, PlayerInfo> thePlayers = dataFetcher.getPlayersFromTeamIds(allTeamIds);
 
-        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        ExecutorService executor = Executors.newFixedThreadPool(dataFetcher.getThreadPoolSize());
         List<Future<?>> futures = new ArrayList<>();
 
-        for (Map.Entry<Long, PlayerInfo> entry : thePlayers.entrySet()) {
-            Long pid = entry.getKey();
+        for (Map.Entry<String, PlayerInfo> entry : thePlayers.entrySet()) {
+            String pid = entry.getKey();
             PlayerInfo pInfo = entry.getValue();
-            futures.add(executor.submit(() -> webDataFetcher.checkPlayer(pid, pInfo)));
+            futures.add(executor.submit(() -> dataFetcher.checkPlayer(pid, pInfo)));
         }
 
         for (Future<?> future : futures) {
@@ -60,8 +83,7 @@ public class PlayerDataService {
         }
         executor.shutdown();
 
-        // TODO: return the results
-        return List.of();
+        return dataFetcher.getCompliantPlayers();
     }
 
 }
