57b86966d989fc554a2305d0807ae9391ab60609
Douglas Vieira <douglasdotv@hotmail.com>
2023-07-31 13:02:21 -0300

    Define WebClient and DocumentBuilder as Spring beans

--- src/main/java/br/com/dv/ntplayersearch/config/AppConfig.java
+++ src/main/java/br/com/dv/ntplayersearch/config/AppConfig.java
@@ -1,2 +1,29 @@
-package br.com.dv.ntplayersearch.config;public class AppConfig {
+package br.com.dv.ntplayersearch.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.create();
+    }
+
+    @Bean
+    public DocumentBuilder documentBuilder() {
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            return dbf.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException("Error while initializing DocumentBuilder", e);
+        }
+    }
+
 }
