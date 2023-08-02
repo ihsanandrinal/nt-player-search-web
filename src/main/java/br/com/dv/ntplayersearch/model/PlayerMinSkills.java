a27dec7da6a32457c2cd494563a4b148b118d651
Douglas Vieira <douglasdotv@hotmail.com>
2023-08-02 06:29:17 -0300

    Change PlayerMinSkills to class to fix form binding issue

--- src/main/java/br/com/dv/ntplayersearch/model/PlayerMinSkills.java
+++ src/main/java/br/com/dv/ntplayersearch/model/PlayerMinSkills.java
@@ -1,18 +1,20 @@
 package br.com.dv.ntplayersearch.model;
 
-public record PlayerMinSkills(
-        Integer speed,
-        Integer stamina,
-        Integer playIntelligence,
-        Integer passing,
-        Integer shooting,
-        Integer heading,
-        Integer keeping,
-        Integer ballControl,
-        Integer tackling,
-        Integer aerialPassing,
-        Integer setPlays,
-        Integer experience,
-        Integer totalBalls
-) {
+import lombok.Data;
+
+@Data
+public class PlayerMinSkills {
+    private Integer speed;
+    private Integer stamina;
+    private Integer playIntelligence;
+    private Integer passing;
+    private Integer shooting;
+    private Integer heading;
+    private Integer keeping;
+    private Integer ballControl;
+    private Integer tackling;
+    private Integer aerialPassing;
+    private Integer setPlays;
+    private Integer experience;
+    private Integer totalBalls;
 }
