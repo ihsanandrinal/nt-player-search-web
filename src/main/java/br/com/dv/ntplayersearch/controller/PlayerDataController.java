e319f24210ac20a6460f0d64890314c9d1948125
Douglas Vieira <douglasdotv@hotmail.com>
2023-08-02 20:01:05 -0300

    Add country codes to model via @ModelAttribute

--- src/main/java/br/com/dv/ntplayersearch/controller/PlayerDataController.java
+++ src/main/java/br/com/dv/ntplayersearch/controller/PlayerDataController.java
@@ -2,23 +2,28 @@ package br.com.dv.ntplayersearch.controller;
 
 import br.com.dv.ntplayersearch.model.Player;
 import br.com.dv.ntplayersearch.model.PlayerSearchForm;
+import br.com.dv.ntplayersearch.service.CountryCodeService;
 import br.com.dv.ntplayersearch.service.PlayerDataService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
 public class PlayerDataController {
 
     private final PlayerDataService playerDataService;
+    private final CountryCodeService countryCodeService;
 
-    public PlayerDataController(PlayerDataService playerDataService) {
+    public PlayerDataController(PlayerDataService playerDataService, CountryCodeService countryCodeService) {
         this.playerDataService = playerDataService;
+        this.countryCodeService = countryCodeService;
     }
 
     @GetMapping("/")
@@ -36,6 +41,11 @@ public class PlayerDataController {
             log.error("Error occurred while searching for players", e);
         }
         return "results";
+    }
+
+    @ModelAttribute("countryCodes")
+    public Map<String, String> getCountryCodes() {
+        return countryCodeService.getCountryCodes();
     }
 
 }
