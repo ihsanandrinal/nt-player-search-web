dcc18be23b334a9d19df1b7d48df54658e6220a3
Douglas Vieira <douglasdotv@hotmail.com>
2023-08-02 21:04:29 -0300

    Change PlayerSearchForm to initialize PlayerMinSkills

--- src/main/java/br/com/dv/ntplayersearch/model/PlayerSearchForm.java
+++ src/main/java/br/com/dv/ntplayersearch/model/PlayerSearchForm.java
@@ -11,5 +11,5 @@ public class PlayerSearchForm {
     private Integer finalLeagueId;
     private Integer minAge;
     private Integer maxAge;
-    private PlayerMinSkills playerMinSkills;
+    private PlayerMinSkills playerMinSkills = new PlayerMinSkills();
 }
