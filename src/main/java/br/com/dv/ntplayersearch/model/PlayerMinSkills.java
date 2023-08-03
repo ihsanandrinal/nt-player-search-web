47031fd15aba118618c136178799907596f6dae6
Douglas Vieira <douglasdotv@hotmail.com>
2023-08-02 21:03:13 -0300

    Initialize PlayerMinSkills class fields with '0' to avoid null object in form

--- src/main/java/br/com/dv/ntplayersearch/model/PlayerMinSkills.java
+++ src/main/java/br/com/dv/ntplayersearch/model/PlayerMinSkills.java
@@ -4,17 +4,17 @@ import lombok.Data;
 
 @Data
 public class PlayerMinSkills {
-    private Integer speed;
-    private Integer stamina;
-    private Integer playIntelligence;
-    private Integer passing;
-    private Integer shooting;
-    private Integer heading;
-    private Integer keeping;
-    private Integer ballControl;
-    private Integer tackling;
-    private Integer aerialPassing;
-    private Integer setPlays;
-    private Integer experience;
-    private Integer totalBalls;
+    private Integer speed = 0;
+    private Integer stamina = 0;
+    private Integer playIntelligence = 0;
+    private Integer passing = 0;
+    private Integer shooting = 0;
+    private Integer heading = 0;
+    private Integer keeping = 0;
+    private Integer ballControl = 0;
+    private Integer tackling = 0;
+    private Integer aerialPassing = 0;
+    private Integer setPlays = 0;
+    private Integer experience = 0;
+    private Integer totalBalls = 0;
 }
