3ac5971504749b5b18e29b4ae6907d4f7b827069
Douglas Vieira <douglasdotv@hotmail.com>
2023-08-12 22:53:51 -0300

    remove unused import

--- src/main/java/br/com/dv/ntplayersearch/model/PlayerSearchForm.java
+++ src/main/java/br/com/dv/ntplayersearch/model/PlayerSearchForm.java
@@ -1,7 +1,6 @@
 package br.com.dv.ntplayersearch.model;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
