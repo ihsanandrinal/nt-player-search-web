2611912db125ade3130ccff7adcd2903200e7602
Douglas Vieira <douglasdotv@hotmail.com>
2023-08-20 12:06:51 -0300

    Create RateLimiterService

--- src/main/java/br/com/dv/ntplayersearch/service/RateLimiterService.java
+++ src/main/java/br/com/dv/ntplayersearch/service/RateLimiterService.java
@@ -1,2 +1,31 @@
-package br.com.dv.ntplayersearch.service;public class RateLimiterService {
+package br.com.dv.ntplayersearch.service;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class RateLimiterService {
+
+    private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
+
+    public Bucket resolveBucket(String ip) {
+        return buckets.computeIfAbsent(ip, this::newBucket);
+    }
+
+    private Bucket newBucket(String ip) {
+        Bandwidth limit = Bandwidth.classic(3, Refill.intervally(3, Duration.ofMinutes(1)));
+        return Bucket.builder().addLimit(limit).build();
+    }
+
+    public boolean isAllowed(String ip) {
+        Bucket bucket = resolveBucket(ip);
+        return bucket.tryConsume(1);
+    }
+
 }
